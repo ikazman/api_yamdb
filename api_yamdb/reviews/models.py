7d34ad5e3b8885724803a887c268531d481090e0
Artyom Oleynik <artyom.oleynik@gmail.com>
2021-08-11 18:59:14 +0200

    Models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,68 +1,90 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
+from datetime import datetime
 
 User = get_user_model()
 
-
-# ▼▼▼
-class Title(models.Model):
-    name = models.CharField(max_length=200)
-    year =
-    category = models.ForeignKey('Category', related_name='comments')  
-
-    def __str__(self):
-        return self.name[:15]
-
-
-# ▼▼▼
-class Comment(models.Model):
-    review_id = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')    
-    text = models.TextField()
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
-    pub_date = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
-
-    def __str__(self):
-        return self.text[:15]
-
-
-# ▼▼▼
 class Category(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True, max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-# ▼▼▼
-class Genre(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True, max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class Follow(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='follower'
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Имя категории',
+        help_text='Введите имя категории'
     )
-    following = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='following'
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Ключевое слово',
+        help_text='Введите ключевое слово',
+        db_index=True
     )
 
     class Meta:
-        constraints = (
-            models.CheckConstraint(
-                name='prevent_self_follow',
-                check=~models.Q(user=models.F('following')),
-            ),
-            models.UniqueConstraint(fields=['user', 'following'],
-                                    name='unique_followers')
-        )
+        ordering = ('name',)
+        verbose_name = 'Ключевое слово'
+        verbose_name_plural = 'Ключевые слова'
 
     def __str__(self):
-        return f'follower: {self.user}, following: {self.following}'
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название',
+        help_text='Введите название'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Ключевое слово',
+        help_text='Введите ключевое слово',
+        db_index=True
+    )
+
+    class Meta:
+        ordering = ('slug',)
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        verbose_name='Произведение',
+        help_text='Введите имя',
+        max_length=100
+    )
+    year = models.PositiveSmallIntegerField(
+        verbose_name='Год',
+        help_text='Введите год',
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(datetime.now().year)
+        ],
+        db_index=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='title'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        blank=True,
+        null=True,
+        related_name='title'
+    )
+
+    class Meta:
+        ordering = ('pk',)
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
