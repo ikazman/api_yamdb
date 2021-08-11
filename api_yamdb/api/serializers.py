e99c5eaa17b7a6543a132fc9cde60a30fe1da717
Artyom Oleynik <artyom.oleynik@gmail.com>
2021-08-11 22:07:03 +0200

    v1

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,4 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
-from rest_framework.validators import UniqueTogetherValidator
 
 from reviews.models import Category, Genre, Title
 
@@ -8,32 +6,27 @@ from reviews.models import Category, Genre, Title
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ("name", "slug")
+        fields = ('name', 'slug',)
 
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
-        fields = ("name", "slug")
+        fields = ('name', 'slug',)
 
 
 class TitlePostSerializer(serializers.ModelSerializer):
     genre = serializers.SlugRelatedField(
-        slug_field="slug", many=True, queryset=Genre.objects.all()
-    )
+        slug_field='slug',
+        many=True,
+        queryset=Genre.objects.all())
     category = serializers.SlugRelatedField(
-        slug_field="slug", queryset=Category.objects.all()
-    )
+        slug_field='slug',
+        queryset=Category.objects.all())
 
     class Meta:
         model = Title
-        fields = (
-            "id",
-            "name",
-            "year",
-            "genre",
-            "category",
-        )
+        fields = ('id', 'name', 'year', 'genre', 'category',)
 
 
 class TitleListSerializer(serializers.ModelSerializer):
@@ -43,10 +36,4 @@ class TitleListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Title
-        fields = (
-            "id",
-            "name",
-            "year",
-            "genre",
-            "category",
-        )
+        fields = ('id', 'name', 'year', 'genre', 'category',)
