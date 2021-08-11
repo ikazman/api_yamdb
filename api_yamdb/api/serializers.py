faf7ac143edec97b4af9bda9b4d7552e9052a84d
Artyom Oleynik <artyom.oleynik@gmail.com>
2021-08-11 20:25:45 +0200

    urls v_1

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -8,27 +8,32 @@ from reviews.models import Category, Genre, Title
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('name', 'slug')
+        fields = ("name", "slug")
 
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
-        fields = ('name', 'slug')
+        fields = ("name", "slug")
 
 
 class TitlePostSerializer(serializers.ModelSerializer):
     genre = serializers.SlugRelatedField(
-        slug_field='slug',
-        many=True,
-        queryset=Genre.objects.all())
+        slug_field="slug", many=True, queryset=Genre.objects.all()
+    )
     category = serializers.SlugRelatedField(
-        slug_field='slug',
-        queryset=Category.objects.all())
+        slug_field="slug", queryset=Category.objects.all()
+    )
 
     class Meta:
         model = Title
-        fields = ('id', 'name', 'year', 'genre', 'category',)
+        fields = (
+            "id",
+            "name",
+            "year",
+            "genre",
+            "category",
+        )
 
 
 class TitleListSerializer(serializers.ModelSerializer):
@@ -38,4 +43,10 @@ class TitleListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Title
-        fields = ('id', 'name', 'year', 'genre', 'category',)
+        fields = (
+            "id",
+            "name",
+            "year",
+            "genre",
+            "category",
+        )
