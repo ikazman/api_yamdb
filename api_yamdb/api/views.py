6831ab565e1c4401ea52c47b2625d2a9e0a995c8
Artyom Oleynik <artyom.oleynik@gmail.com>
2021-08-15 02:29:02 +0200

    update views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,15 +1,7 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.core.mail import send_mail
 from django.db.models import Avg
-from django.shortcuts import get_object_or_404
-from rest_framework import filters, mixins, status, viewsets
+from rest_framework import filters, mixins, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.filters import SearchFilter
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import AccessToken
-
 from .filters import TitleFilter
 from reviews.models import Category, Genre, Title
 from .serializers import (
